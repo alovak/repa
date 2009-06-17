fa39853ffa5b05f1ccd17fe0d7732a1b1d5f00f2
Pavel Gabriel <alovak@gmail.com>
2009-06-18 01:56:39 +0300

    method 'deep_symbolize_keys' was added

--- lib/ext/core.rb
+++ lib/ext/core.rb
@@ -3,3 +3,13 @@ class Symbol
     [self]
   end
 end
+
+class Hash
+  def deep_symbolize_keys
+      self.inject({}) do |result, (key, value)|
+      value = value.deep_symbolize_keys if value.is_a? Hash
+      result[(key.to_sym rescue key) || key] = value
+      result
+    end
+  end
+end
