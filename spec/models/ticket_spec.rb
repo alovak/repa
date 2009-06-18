5805a82dd3a0c40094e6a21d427620a09a9ef638
Pavel Gabriel <alovak@gmail.com>
2009-06-18 16:23:37 +0300

    all 'bad' specs were deleted

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -10,14 +10,4 @@ describe Ticket do
   it "should create a new instance given valid attributes" do
     Ticket.create!(@valid_attributes)
   end
-
-  it "should allow event" do
-    @ticket.state = :new
-    @ticket.allow_event?(:approve).should be_true
-  end
-
-  it "should not allow event" do
-    @ticket.state = :new
-    @ticket.allow_event?(:test).should be_false
-  end
 end
