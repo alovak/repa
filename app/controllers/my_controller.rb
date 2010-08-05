e804896d8e3947b304a72a3773accba587473788
Pavel Gabriel <alovak@gmail.com>
2010-08-05 16:46:03 +0300

    small change

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -27,7 +27,7 @@ class MyController < ApplicationController
 
   def close_works
     works_count = 0
-    Work.find(:all, :conditions => ["created_at < ? and ( status = 1 or status = 2 or status = 3)", Time.today]).each do |work|
+    Work.find(:all, :conditions => ["created_at < ? and ( status = 1 or status = 2 or status = 3)", Time.zone.now]).each do |work|
       work.auto_close( 1 )
       works_count += 1
     end
